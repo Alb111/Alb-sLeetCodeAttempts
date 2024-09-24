801f0a1fe0ac73433a5b437a7d443c8dc4104ed2
Alb123 <albertfelix2005@gmail.com>
2024-09-24 10:26:50 -0700

    works, uses dicts to skip loops

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -5,30 +5,31 @@
 #
 
 # @lc code=start
+from typing import Dict
+from typing import List
+
+
 class Solution:
     def createDict(self, str):
         temp = [0]*26
         for char in str:
             temp[ord(char)-97]+=1
-        return temp
+        return tuple(temp)
  
 
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        out = []
-        used = [False] * len(strs)
-        for i in range(len(strs)):
-            temp=[]
-            if(not used[i]):
-                str1 = self.createDict(strs[i])
-                temp.append(strs[i])
-                used[i] = True
-                for k in range(i+1, len(strs)):
-                    str2 = self.createDict(strs[k])
-                    if(str1 == str2):
-                        temp.append(strs[k])
-                        used[k] = True
-                out.append(temp)
-        return out
+        out: Dict[tuple, List[str]] = {}
+        for str in strs:
+            temp = self.createDict(str);
+            if( temp in out ):
+                out[temp].append(str)
+            else:
+                out[temp] = []
+                out[temp].append(str)
+        
+        return list(out.values())
+
+
 
                 
 
