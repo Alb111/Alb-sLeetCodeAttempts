3d08d8c941025244e392f9b60da6830bbf08be69
Alb123 <albertfelix2005@gmail.com>
2024-11-07 11:05:58 -0800

    nlogn solution, binary serach'

--- 167.two-sum-ii-input-array-is-sorted.py
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -7,11 +7,24 @@
 # @lc code=start
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        numtoIndex ={}
         for index, num in enumerate(numbers):
-            if((target-num) in numtoIndex):
-                return [numtoIndex[target-num]+1, index+1]
-            numtoIndex[num] = index
-        
+            goal = target - num;
+            # no reason to search for a nums that cant be in the list
+            # if not (target>numbers[-1] or target<0):
+            left, right = index + 1, len(numbers)-1
+
+            while(left <= right):
+                mid = left + (right - left)//2
+                if numbers[mid] == goal:
+                    # found match
+                    return [index + 1, mid + 1]
+                elif numbers[mid] > goal:
+                    right = mid-1
+                else:
+                    left = mid + 1
+                # no match found go to next element
+
+
+       
 # @lc code=end
 
