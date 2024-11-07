9135661d756fe84caa945a5fd717b6c991223a06
Alb123 <albertfelix2005@gmail.com>
2024-11-07 11:11:29 -0800

    two pointer soltuions

--- 167.two-sum-ii-input-array-is-sorted.py
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -7,24 +7,19 @@
 # @lc code=start
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for index, num in enumerate(numbers):
-            goal = target - num;
-            # no reason to search for a nums that cant be in the list
-            # if not (target>numbers[-1] or target<0):
-            left, right = index + 1, len(numbers)-1
+        left, right = 0, len(numbers) - 1
+        cursum = -100
+        while ( cursum != target):
+            cursum = numbers[left] + numbers[right]
+            if cursum > target:
+                right-=1
+            elif cursum < target:
+                left+=1
+            else:
+                return [left+1, right+1]            
+            
 
-            while(left <= right):
-                mid = left + (right - left)//2
-                if numbers[mid] == goal:
-                    # found match
-                    return [index + 1, mid + 1]
-                elif numbers[mid] > goal:
-                    right = mid-1
-                else:
-                    left = mid + 1
-                # no match found go to next element
-
-
+      
        
 # @lc code=end
 
